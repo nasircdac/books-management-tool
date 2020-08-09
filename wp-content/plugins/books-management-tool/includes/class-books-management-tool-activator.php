625b6dd6c85272f03c0bc989369d5761df4b89d9
Nasir Khan <nasir@qs.com>
2020-08-09 13:25:47 +0530

    books management tool

---
+++ wp-content/plugins/books-management-tool/includes/class-books-management-tool-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://localhost/multisite/
+ * @since      1.0.0
+ *
+ * @package    Books_Management_Tool
+ * @subpackage Books_Management_Tool/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Books_Management_Tool
+ * @subpackage Books_Management_Tool/includes
+ * @author     Nasir Khan <nasircdac2019@gmail.com>
+ */
+class Books_Management_Tool_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ wp-content/plugins/books-management-tool/includes/class-books-management-tool-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://localhost/multisite/
+ * @since      1.0.0
+ *
+ * @package    Books_Management_Tool
+ * @subpackage Books_Management_Tool/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Books_Management_Tool
+ * @subpackage Books_Management_Tool/includes
+ * @author     Nasir Khan <nasircdac2019@gmail.com>
+ */
+class Books_Management_Tool_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
